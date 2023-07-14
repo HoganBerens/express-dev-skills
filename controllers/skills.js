012a2365b2fbe16d30790afb9b4ff7229bb57112
HoganBerens <berenshogan@gmail.com>
2023-07-13 21:11:45 -0500

    add new skill button and ability to update existing skills

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,16 +3,55 @@ const Skill = require("../models/skill");
 function index(req, res) {
   res.render("skills/index", {
     skills: Skill.getAll(),
+    title: "All Skills",
   });
 }
 
 function show(req, res) {
   res.render("skills/show", {
     skill: Skill.getOne(req.params.id),
+    title: "Skill Details",
   });
+}
+
+function newSkill(req, res) {
+  res.render("skills/new", {
+    title: "New skill",
+  });
+}
+
+function create(req, res) {
+  Skill.create(req.body);
+  res.redirect("/skills");
+}
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect("/skills");
+}
+
+function edit(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render("skills/edit", {
+    title: "Edit Skill",
+    skill,
+  });
+  res.redirect("/skills");
+}
+
+function update(req, res) {
+  req.body.has = !!req.body.has;
+  Skill.update(req.params.id, req.body);
+
+  res.redirect(`/skills/${req.params.id}`);
 }
 
 module.exports = {
   index,
   show,
+  new: newSkill,
+  create,
+  delete: deleteSkill,
+  edit,
+  update,
 };
--- models/skill.js
+++ models/skill.js
@@ -4,17 +4,37 @@ const skills = [
   { id: 1398, skill: "Software Design", has: true },
   { id: 1398, skill: "Self Movtivated", has: true },
 ];
-
-function getAll() {
-  return skills;
-}
-
 function getOne(id) {
   id = parseInt(id);
   return skills.find((skill) => skill.id === id);
 }
 
+function getAll() {
+  return skills;
+}
+
+function create(skill) {
+  skill.id = Date.now() % 1000000;
+  skill.has = false;
+  skills.push(skill);
+}
+
+function deleteOne(id) {
+  id = parseInt(id);
+  const idx = skills.findIndex((skill) => skill.id === id);
+  skills.splice(idx, 1);
+}
+
+function update(id, updatedSkill) {
+  id = parseInt(id);
+  const skill = skills.find((skill) => skill.id === id);
+  Object.assign(skill, updatedSkill);
+}
+
 module.exports = {
   getAll,
   getOne,
+  create,
+  deleteOne,
+  update,
 };
